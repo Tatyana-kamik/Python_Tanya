057cdd30c9379c520734952154a4cde3c1158489
tatyana-kamik <kamikadze6240@ya.ru>
2025-07-02 17:34:43 +0300

    Исправлен код в соответствии PEP8

--- 02_lesson/lesson_2_task_3.py
+++ 02_lesson/lesson_2_task_3.py
@@ -1,10 +1,11 @@
 import math
 
 def square(side):
-   
+    
     area = side ** 2
-    return math.ceil(area)  
+    return math.ceil(area)
 
-side_length = 3.6 
+side_length = 3.6
 area_result = square(side_length)
+
 print(f"Площадь квадрата со стороной {side_length}: {area_result}")
--- 02_lesson/lesson_2_task_6.py
+++ 02_lesson/lesson_2_task_6.py
@@ -1,5 +1,9 @@
 lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
 
-filtered_elements = [num for num in lst if num < 30 and num % 3 == 0]
+# Фильтрация элементов списка
+filtered_elements = [
+    num for num in lst if num < 30 and num % 3 == 0
+]
 
+# Вывод отфильтрованных элементов
 print(filtered_elements)
