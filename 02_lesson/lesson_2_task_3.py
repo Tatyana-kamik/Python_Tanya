364a30aca6ed569539ef2d5a5007a05a391aaf29
tatyana-kamik <kamikadze6240@ya.ru>
2025-07-02 13:01:17 +0300

    Lesson2

---
+++ 02_lesson/lesson_2_task_3.py
@@ -0,0 +1,10 @@
+import math
+
+def square(side):
+   
+    area = side ** 2
+    return math.ceil(area)  
+
+side_length = 3.6 
+area_result = square(side_length)
+print(f"Площадь квадрата со стороной {side_length}: {area_result}")
---
+++ 02_lesson/lesson_2_task_5.py
@@ -0,0 +1,17 @@
+def month_to_season(month):
+   
+    if month < 1 or month > 12:
+        return "Некорректный месяц. Введите число от 1 до 12."
+    
+    if month in (12, 1, 2):
+        return "Зима"
+    elif month in (3, 4, 5):
+        return "Весна"
+    elif month in (6, 7, 8):
+        return "Лето"
+    elif month in (9, 10, 11):
+        return "Осень"
+
+month = 7
+season = month_to_season(month)
+print(season) 
