5808ab94739992a4376eaf7bdd36cb721e48402e
tatyana-kamik <kamikadze6240@ya.ru>
2025-07-10 16:27:06 +0300

    Домашнее задание 3

--- 03_lesson/lesson_3_task_1.py
+++ 03_lesson/lesson_3_task_1.py
@@ -2,6 +2,6 @@ from user import User
 
 my_user = User("Татьяна", "Байцурова")
 
-my_user.print_first_name()  # Выводит: Иван
-my_user.print_last_name()   # Выводит: Иванов
-my_user.print_full_name()   # Выводит: Иван Иванов
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
